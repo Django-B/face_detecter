e5b715c091ce879ec49cf171aaaa4fb4b1891f32
Django-B <django.ppp@gmail.com>
2022-11-22 20:50:47 +0300

    -

--- main.py
+++ main.py
@@ -2,33 +2,6 @@
 import cv2
 
 
-# cascade_path = '/home/django/code/camera_ai/filters/haarcascade_frontalface_default.xml'
-# clf = cv2.CascadeClassifier(cascade_path)
-
-# # define a video capture object
-# camera = cv2.VideoCapture(0)
-
-# while True:
-# 	# Capture the video frame
-# 	# by frame
-# 	ret, frame = camera.read()
-
-
-# 	# Display the resulting frame
-# 	cv2.imshow('frame', frame)
-	
-# 	# the 'q' button is set as the
-# 	# quitting button you may use any
-# 	# desired button of your choice
-# 	if cv2.waitKey(1) & 0xFF == ord('q'):
-# 		break
-
-# # After the loop release the cap object
-# camera.release()
-# # Destroy all the windows
-# cv2.destroyAllWindows()
-
-
 def detect_face():
     cascade_path = '/home/django/code/camera_ai/filters/haarcascade_frontalface_default.xml'
     # cascade_path = '/home/django/code/camera_ai/filters/haarcascade_eye.xml'
@@ -46,7 +19,7 @@ def detect_face():
         faces = clf.detectMultiScale(
             gray, # Image
             scaleFactor=1.1, # Коэфицент масштабирования
-            minNeighbors=100, # Минимальное число соседей, чем больше, тем строже критерии(найдется меньше лиц) и наоборот, рекомендуемо 5
+            minNeighbors=5, # Минимальное число соседей, чем больше, тем строже критерии(найдется меньше лиц) и наоборот, рекомендуемо 5
             minSize=(30, 30), # Минимальный размер объекта
             flags=cv2.CASCADE_SCALE_IMAGE, # Нужно по документации
         )
